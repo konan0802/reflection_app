8ba23dc34b3e4c6954d820ba71c2b1f2662ffcac
konan080211 <konanforbis@gmail.com>
2022-11-05 16:22:01 +0900

    fix: ログイン画面のビルド

--- FE/lib/SignPage.dart
+++ FE/lib/SignPage.dart
@@ -1,17 +1,48 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
-class SignPage extends StatelessWidget {
+import 'amplifyconfiguration.dart';
+
+class SignPage extends StatefulWidget {
   const SignPage({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() {
+    return _SignPageState();
+  }
+}
+
+class _SignPageState extends State {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+      print('Successfully configured');
+    } on Exception catch (e) {
+      print('Error configuring Amplify: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "aiueo",
-        style: TextStyle(
-            fontSize: 80, fontWeight: FontWeight.w600, color: Colors.white),
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('ホーム')),
+          body: const Center(
+            child: Text('ログイン成功！', style: TextStyle(fontSize: 32.0)),
+          ),
+        ),
       ),
-      backgroundColor: Color.fromRGBO(206, 229, 208, 1),
     );
   }
 }
